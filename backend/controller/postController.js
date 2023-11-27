10f584055bfd1d3943a1e04740d0ac7aed8112e7
ritik2727 <ritikjain2727@gmail.com>
2023-11-27 15:24:42 +0530

    add getPostById and delete post routes

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,16 +1,61 @@
-const asyncHandler = require('express-async-handler');
-const postModel = require('../models/postModel');
+const asyncHandler = require("express-async-handler");
+const postModel = require("../models/postModel");
 
-exports.createPost = asyncHandler(async(req,res)=>{
-    console.log(req.profile);
+exports.createPost = asyncHandler(async (req, res) => {
+  console.log(req.profile);
 
-    const post = await postModel.create({
-        ...req.body,
-        profile:req.profile,
-        user:req.user.id,
-    })
-    res.status(200).json({
-        status:'success',
-        post
-    })
-})
+  const post = await postModel.create({
+    ...req.body,
+    profile: req.profile,
+    user: req.user.id,
+  });
+  res.status(200).json({
+    status: "success",
+    post,
+  });
+});
+
+exports.getAllPosts = asyncHandler(async (req, res) => {
+  const posts = await postModel
+    .find({})
+    .sort({ createdAt: "descending" })
+    .populate("profile")
+    .limit(20);
+  // const populatedPost = await posts.populate('profile').execPopulate();
+  res.status(200).json({
+    status: "success",
+    data: posts.length,
+    posts,
+  });
+});
+
+exports.getPostById = asyncHandler(async (req, res) => {
+  const post = await postModel.findById(req.params.id);
+
+  if (!post) {
+    res.status(404);
+    throw new Error("No post found");
+  }
+
+  res.status(200).json({
+    status: "success",
+    post,
+  });
+});
+
+exports.deletePostById = asyncHandler(async (req, res) => {
+  const post = await postModel.findById(req.params.id);
+
+  if (!post) {
+    res.status(404);
+    throw new Error("No post found");
+  }
+//   console.log(post.user.toString(),req.user._id)
+
+  if (post.user.toString() != req.user._id.toString()) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+  await postModel.deleteOne({_id:post._id});
+  res.status(200).json({ success: true, message: "Post deleted successfully" });
+});
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createPost } = require('../controller/postController');
+const { createPost, getAllPosts, getPostById, deletePostById } = require('../controller/postController');
 const { protect, getProfileId } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/',protect,getProfileId,createPost);
+router.route('/').post(protect,getProfileId,createPost).get(protect,getAllPosts)
+router.route('/:id').get(getPostById).delete(protect,deletePostById);
 
 
 module.exports = router
